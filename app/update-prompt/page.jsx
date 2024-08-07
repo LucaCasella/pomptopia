f505fb0d444422ca1d78bd19c340998db5e78b44
Luca Casella <luca.casella@casella.local>
2024-08-07 14:31:20 +0200

    fix2

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,32 +1,40 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
 
 import Form from '@/components/Form';
 
-const EditPrompt = () => {
+const FallbackComponent = () => {
+  return <div>Loading...</div>;
+};
+
+const EditPromptContent = ({ promptId }) => {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get('id');
-console.log(promptId)
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
     tag: '',
-  })
+  });
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
+      try {
+        const response = await fetch(`/api/prompt/${promptId}`);
+        const data = await response.json();
 
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    }
+        setPost({
+          prompt: data.prompt,
+          tag: data.tag,
+        });
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
     if (promptId) getPromptDetails();
   }, [promptId]);
@@ -38,38 +46,50 @@ console.log(promptId)
     if (!promptId) {
       return alert('Prompt ID not found');
     }
-    console.log(promptId)
+    console.log(promptId);
     try {
-      const response = await fetch(`/api/prompt/${promptId}`, 
-      {
+      const response = await fetch(`/api/prompt/${promptId}`, {
         method: 'PATCH',
         body: JSON.stringify({
           prompt: post.prompt,
           tag: post.tag
-        })
-      })
-      console.log(response)
+        }),
+      });
+      console.log(response);
       if (response.ok) {
         router.push('/');
       }
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     } finally {
       setSubmitting(false);
     }
+  };
+
+  if (loading) {
+    return <FallbackComponent />;
   }
 
   return (
-    <Suspense>
-      <Form
+    <Form
       type='Edit'
       post={post}
       setPost={setPost}
       submitting={submitting}
       handleSubmit={updatePrompt}
     />
-    </Suspense>
-  )
-}
+  );
+};
 
-export default EditPrompt
+const EditPrompt = () => {
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get('id');
+
+  return (
+    <Suspense fallback={<FallbackComponent />}>
+      <EditPromptContent promptId={promptId} />
+    </Suspense>
+  );
+};
+
+export default EditPrompt;
